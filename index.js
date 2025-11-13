2521ab87188399dea2453d971dc9b47a6454009c
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-11-13 23:22:01 +0700

    Add limit on fetching ideas

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 app.use(errorHandler)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 app.listen(port, () => {
   console.log(`App listen http://localhost:${port}`)
 })
