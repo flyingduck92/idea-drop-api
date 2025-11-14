34c39c38e19fbfa742304b94b39b0d0fc13b3b54
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-11-14 21:43:35 +0700

    Add register route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,38 @@
 import express from 'express'
-import mongoose from 'mongoose'
 import User from '../models/User.js'
 
 const authRoutes = express.Router()
+
+// @route           GET /api/auth/register
+// @description     Register new user
+// @access          Public
+authRoutes.post('/register', async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+      res.status(400)
+      throw new Error('All fields are required')
+    }
+
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+      res.status(400)
+      throw new Error('User  already exists.')
+    }
+
+    const user = await User.create({ name, email, password })
+    return res.status(201).json({
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    })
+  } catch (err) {
+    console.log(err)
+    next(err)
+  }
+})
 
 export default authRoutes
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import ideaRoutes from './ideaRoutes.js'
+import authRoutes from './authRoutes.js'
 
 const mainApiRoutes = express.Router()
 const base = '/api'
 
 mainApiRoutes.use(`${base}/ideas`, ideaRoutes)
+mainApiRoutes.use(`${base}/auth`, authRoutes)
 
 export default mainApiRoutes
